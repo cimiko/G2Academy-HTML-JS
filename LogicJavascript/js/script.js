63186c3016cd51ce2464c8041b3897515971fec1
CiMiKo <private.hartono@mail.com>
2021-01-16 14:50:44 +0700

    Menambah script cara lain

--- LogicJavascript/js/script.js
+++ LogicJavascript/js/script.js
@@ -25,14 +25,42 @@ for (let i = 0; i < 5; i++) {
 const generateDot2 = _ =>{
     const dot = `<div class="dot"></div>`
 
-    let dt = "";
     for (let i = 0; i < 5; i++) {
         document.querySelector("div.boxDots").innerHTML += dot;
         for (let j = 0; j < 4; j++) {
-            dt += dot.outerHTML
             document.querySelector("div.boxDots").innerHTML += dot;
         }
         document.querySelector("div.boxDots").innerHTML += "<br>"
     }
 }
 generateDot2()
+
+// const generateDot3 = _ =>{
+//     const dot = `<div class="dot"></div>`
+
+//     let dt = "";
+//     for (let i = 0; i < 5; i++) {
+//         document.querySelector("div.boxDots").innerHTML += dot;
+//         for (let j = 0; j < 4; j++) {
+//             dt += dot.outerHTML
+//             document.querySelector("div.boxDots").innerHTML += dot;
+//         }
+//         document.querySelector("div.boxDots").innerHTML += "<br>"
+//     }
+// }
+// generateDot3()
+
+// const generateDot4 = _ =>{
+//     const dot = `<div class="dot"></div>`
+//     const box = document.querySelector(".boxDots")
+
+//     let dt = "";
+//     for (let i = 0; i < 5; i++) {
+//         for (let j = 0; j < 5; j++) {
+//             dt += dot
+//         }
+//         dt += "<br>"
+//     }
+//     box.innerHTML = dt
+// }
+// generateDot4()
