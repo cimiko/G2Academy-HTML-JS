013dd042d264bbe1a69134cfe4cfe2f360ac3120
CiMiKo <private.hartono@mail.com>
2021-01-18 20:20:07 +0700

    FInal

--- LogicJavascript/js/script.js
+++ LogicJavascript/js/script.js
@@ -1,4 +1,5 @@
 
+
 // for (let i = 0; i < 5; i++) {
 //     document.querySelector('.logic').innerHTML += "test "
 //     for (let j = 0; j < 5; j++) {
@@ -281,24 +282,6 @@ const typing = () => {
 }
 // typing();
 
-
-const setDate = () => {
-    const hourHand = document.querySelector('.hour');
-    const minuteHand = document.querySelector('.minute');
-    const secondHand = document.querySelector('.second');
-    const now = new Date();
-    
-    const minuteDegrees = ((now.getMinutes() / 60) * 360) + 180;
-    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
-    
-    const hourDegrees = ((now.getHours()/12) * 360) + 180;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-    const secondDegrees = ((now.getSeconds() / 60) * 360) + 180;
-    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-    setInterval(setDate, 1000);
-}
-// setDate()
 
 
 
